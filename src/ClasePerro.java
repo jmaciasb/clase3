403232e4d8c166d9e9d39142dd50974153e6e9a4
jmaciasb <jmaciasb@BE401S103C29ING.unal.loc>
2019-04-10 08:57:26 -0500

    Clase 10/04

---
+++ src/ClasePerro.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jmaciasb
+ */
+public class ClasePerro {
+    public String nombre;
+    public int edad;
+    public String color;
+    public double peso;
+    
+    public void comer(){
+    }
+    public void dormir(){
+    }
+    public void ladrar(){
+    }
+    public void correr(){
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jmaciasb
+ */
+public class Main {
+    public static void main(String[] args) {
+        ClasePerro Doggo = new ClasePerro();
+        Doggo.nombre = "Doggo";
+        System.out.println(Doggo.nombre);
+    }
+}
